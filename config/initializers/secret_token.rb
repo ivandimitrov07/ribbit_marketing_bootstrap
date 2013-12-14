d202a153c1b1a94e0bac805cae958d91051e88d9
ivandimitrov07 <idimitrov07@yahoo.com>
2013-12-14 19:09:42 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RibbitMarketingBootstrap::Application.config.secret_token = '01afc279874e5c22c5e326e11e15e7988640d6421e604caa8eda8c31576eadbc3c02372c497a5b576b0e492346b61591d50182a3742e7df7d3bb5d70b87c286f'
